e2b533b018b5db13a737154f0ce3edae4bed3336
jina <rladpwlsk@naver.com>
2024-01-24 20:04:42 +0900

    c_study_edit

--- 2-2.c
+++ 2-2.c
@@ -1,15 +1,15 @@
-//#include <stdio.h>
-//
-//int main(){
-//	int a = 6;
-//	int b = 13;
-//	char c = 'c';
-//	
-//	int *pa = &a;
-//	int *pb = &b;
-//	char *pc = &c;
-//	
-//	int c = *pa + *pb;
-//	printf("%d",c);
-//	return 0;
-//} 
+#include <stdio.h>
+
+int main(){
+	int a = 6;
+	int b = 13;
+	char c = 'c';
+	
+	int *pa = &a;
+	int *pb = &b;
+	char *pc = &c;
+	
+	int c = *pa + *pb;
+	printf("%d",c);
+	return 0;
+} 
--- 2-3.c
+++ 2-3.c
@@ -1,40 +1,40 @@
-//#include <stdio.h>
-//struct A{
-//	int eng; //4byte
-//	int math[3]; //12byte
-//	
-//};
-//int main(){
-//	
-//	struct A a[2];
-//	
-//	a[0].eng=45; 
-//	a[0].math[0]=50;
-//	a[0].math[1]=100;
-//	a[0].math[2]=150;
-//	
-//	a[1].eng=100;
-//	a[1].math[0]=55;
-//	a[1].math[1]=105;
-//	a[1].math[2]=155;
-//	
-//	struct A *b; //b에 저장되는 것은 주소, struct A 의 데이터 타입의 주소 
-//	b = a;
-//	// int z = 11;
-//	// b = &z; 이 식은 허용하지 않음
-//	 
-//	int i=0;
-//	for(i=0;i<2;i++){
-//		printf("%d %d %d \t", *(a[i].math), *(a[i].math+1), *(a[i].math+2)); //a.math * => 역참조 
-//		printf("%d\n", a[i].eng); 
-//		// 포인터는 주소이고 주소에 1을 더하면 그 크기만큼 이동됨 
-//	}
-//	printf("=========================\n");
-//	for(i=0;i<2;i++){
-//		printf("%d %d %d \t", *(b[i].math), *(b[i].math+1), *(b[i].math+2)); //a.math * => 역참조 
-//		printf("%d\n", b[i].eng); 
-//		// 포인터는 주소이고 주소에 1을 더하면 그 크기만큼 이동됨 
-//	}
-//	
-//	return 0;
-//}
+#include <stdio.h>
+struct A{
+	int eng; //4byte
+	int math[3]; //12byte
+	
+};
+int main(){
+	
+	struct A a[2];
+	
+	a[0].eng=45; 
+	a[0].math[0]=50;
+	a[0].math[1]=100;
+	a[0].math[2]=150;
+	
+	a[1].eng=100;
+	a[1].math[0]=55;
+	a[1].math[1]=105;
+	a[1].math[2]=155;
+	
+	struct A *b; //b에 저장되는 것은 주소, struct A 의 데이터 타입의 주소 
+	b = a;
+	// int z = 11;
+	// b = &z; 이 식은 허용하지 않음
+	 
+	int i=0;
+	for(i=0;i<2;i++){
+		printf("%d %d %d \t", *(a[i].math), *(a[i].math+1), *(a[i].math+2)); //a.math * => 역참조 
+		printf("%d\n", a[i].eng); 
+		// 포인터는 주소이고 주소에 1을 더하면 그 크기만큼 이동됨 
+	}
+	printf("=========================\n");
+	for(i=0;i<2;i++){
+		printf("%d %d %d \t", *(b[i].math), *(b[i].math+1), *(b[i].math+2)); //a.math * => 역참조 
+		printf("%d\n", b[i].eng); 
+		// 포인터는 주소이고 주소에 1을 더하면 그 크기만큼 이동됨 
+	}
+	
+	return 0;
+}
